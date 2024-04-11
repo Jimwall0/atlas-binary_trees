d05e4115920a5780d15acde4a8e540e7a43220a4
Tsimmuaj <tyang4204@gmail.com>
2024-04-11 14:04:09 -0500

    checked left branch

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node = NULL;
 
 	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL)
+	if (node == NULL || parent == NULL || parent->left != NULL)
 	{
 		free(node);
 		return (NULL);
