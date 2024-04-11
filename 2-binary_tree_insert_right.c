eeb70550ca34203c31796b6612af910b55769d14
Tsimmuaj <tyang4204@gmail.com>
2024-04-11 15:14:12 -0500

    Added right inser

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - insert on the right node of parent
+ * @parent: node to insert at
+ * @value: data type of node
+ * Return: node at right
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL && parent == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	if (node->parent->right == NULL)
+		node->parent->right = node;
+	else
+	{
+		node->parent->right->parent = node;
+		node->right = node->parent->right;
+		node->parent->right = node;
+	}
+	return (node);
+}
