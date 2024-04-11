0db991d134faa12967239a55e348cbd1171e8c12
Tsimmuaj <tyang4204@gmail.com>
2024-04-11 14:24:23 -0500

    check for existing left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,15 +10,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node = NULL;
 
 	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL || parent->left != NULL)
+	if (node == NULL || parent == NULL)
 	{
 		free(node);
 		return (NULL);
 	}
-	node->parent = parent;
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		node->left->parent = node;
+	}
 	parent->left = node;
+	node->parent = parent;
 	node->n = value;
-	node->left = NULL;
-	node->right = NULL;
 	return (node);
 }
