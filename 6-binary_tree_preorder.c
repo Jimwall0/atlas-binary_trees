6be9460d0befaf76fdd5985ff9cd0ee4ae9bdcf5
Tsimmuaj <tyang4204@gmail.com>
2024-04-16 14:31:25 -0500

    added comments

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - 
+ * binary_tree_preorder - traverses and prints data
+ * @tree: current node
+ * @func: pointer function
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
