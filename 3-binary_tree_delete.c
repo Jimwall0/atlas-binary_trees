3085485523102d9ea6eab69dc3e073e681c0c327
Tsimmuaj <tyang4204@gmail.com>
2024-04-16 13:38:17 -0500

    changed conditional

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,9 +5,10 @@
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-		exit(0);
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->right);
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
 	free(tree);
 }
