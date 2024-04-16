8bcff1e79620f28663c77d3a6bf7e61ee88ce4eb
Tsimmuaj <tyang4204@gmail.com>
2024-04-16 13:59:47 -0500

    separate node check for NULL

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,7 +6,9 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right == NULL && node->left == NULL && node != NULL && node->parent != NULL)
+	if (node == NULL)
+		return (0);
+	if (node->right == NULL && node->left == NULL)
 		return (1);
 	return (0);
 }
