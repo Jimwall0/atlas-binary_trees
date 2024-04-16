6313b5e85fb358033303b6e313c069b24c543f16
Tsimmuaj <tyang4204@gmail.com>
2024-04-16 14:34:58 -0500

    added inorder function

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_tree.h"
+/**
+ * binary_tree_inorder - prints data in inorder
+ * @tree: current node
+ * @func: function pointer to print
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
