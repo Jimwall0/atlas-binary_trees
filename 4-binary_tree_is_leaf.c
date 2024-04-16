681e67c3ba49a8fa94b9ff612e17026e1b95acb1
Tsimmuaj <tyang4204@gmail.com>
2024-04-16 13:53:19 -0500

    changed to check for children

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,7 +6,7 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
-		return (0);
-	return (1);
+	if (node->right != NULL && node->left != NULL)
+		return (1);
+	return (0);
 }
