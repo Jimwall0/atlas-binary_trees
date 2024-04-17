b0f37b79118fb7d69ade20149665b5c5b015dea9
Tsimmuaj <tyang4204@gmail.com>
2024-04-17 16:13:52 -0500

    added total node function

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - counts the amount of nodes
+ * @tree: start from this node
+ * Return: number of total nodes
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
