d3fcedcf011016baaf247f18a4741542399b6c7b
Tsimmuaj <tyang4204@gmail.com>
2024-04-17 19:24:52 -0500

    rebuilt height

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - checks for the balance of a binary tree
+ * @tree: the tree being checked
+ * Return: the balance of the tree
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left = 0, right = 0, total = 0;
+
+	if (tree == NULL)
+		return (0);
+	else if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left < right)
+		left++;
+	else
+		right++;
+	total = left - right;
+	return (total);
+}
