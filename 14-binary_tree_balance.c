27815901e03186ff6dcf72691c230ffcb861b0fb
Tsimmuaj <tyang4204@gmail.com>
2024-04-17 20:47:08 -0500

    testing different full function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,21 +14,3 @@ int binary_tree_balance(const binary_tree_t *tree)
 	right = height(tree->right);
 	return (left - right);
 }
-/**
- * height - height of a binary node
- * @tree: current node
- * Return: balance
-*/
-int height(binary_tree_t *tree)
-{
-	int left = 0, right = 0;
-
-	if (tree == NULL)
-		return (0);
-	left = height(tree->left);
-	right = height(tree->right);
-	if (left > right)
-		return (left + 1);
-	else
-		return (right + 1);
-}
