96a8a713e5e3b3615120bc6db6215225556cd462
Tsimmuaj <tyang4204@gmail.com>
2024-04-18 18:50:35 -0500

    added uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - checks for uncle branch
+ * @node: current branch being checked
+ * Return: the uncle branch
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	int number;
+
+	if (node->parent == NULL || node == NULL)
+		return (NULL);
+	node = node->parent;
+	number = node->n;
+	if (node->parent == NULL || node == NULL)
+		return (NULL);
+	node = node->parent;
+	if (node->left == NULL || node->right == NULL)
+		return (NULL);
+	if (node->left->n == number)
+		return (node->right);
+	else
+		return (node->left);
+}
