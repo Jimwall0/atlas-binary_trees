14b88709cc8c100e365ae299d93dd8727e22f5d3
Tsimmuaj <tyang4204@gmail.com>
2024-08-16 08:22:52 -0500

    added files

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_tree.h"
+/**
+ * binary_tree_node - makes a new node in a binary tree
+ * @parent: parent of the node
+ * @value: int data for the new node
+ * Return: pointer to new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	
+}
